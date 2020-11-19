08ddec624afbc8542b22a981839965ba0c9724e6
Nikola M <nikolamilovic2001@gmail.com>
2020-11-19 18:56:02 +0100

    Adding folder chooser

---
+++ folder_chooser/src/main/java/com/nikolam/folder_chooser/presenter/FolderChooserFragment.kt
@@ -0,0 +1,32 @@
+package com.nikolam.folder_chooser.presenter
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nikolam.folder_chooser.R
+
+class FolderChooserFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FolderChooserFragment()
+    }
+
+    private lateinit var viewModel: FolderChooserViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.folder_chooser_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FolderChooserViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ folder_chooser/src/main/java/com/nikolam/folder_chooser/presenter/FolderChooserViewModel.kt
@@ -0,0 +1,7 @@
+package com.nikolam.folder_chooser.presenter
+
+import androidx.lifecycle.ViewModel
+
+class FolderChooserViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
